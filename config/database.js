c7be617b3b3feb1a9cc9db66b57d8aa34bd7f87b
slmgnm <saleemghanem@gmail.com>
2023-01-23 21:48:19 +0200

    .env

--- config/database.js
+++ config/database.js
@@ -3,15 +3,13 @@ require("dotenv").config();
 
 const mongoose = require("mongoose");
 let db = mongoose.connect(
-  
-    process.env.MONGODB_URI || "mongodb://localhost:27017/eventsDB",
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    (err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("connected to db succcesfuly...");
-      }
-    },
-  );
-
+  process.env.MONGODB_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("connected to db succcesfuly...");
+    }
+  }
+);
