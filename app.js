450b2ee081053ba38f29190746c683b2b49283b1
slmgnm <saleemghanem@gmail.com>
2023-01-24 17:41:11 +0200

    views

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ require("./startup/prod")(app);
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.set("public", path.join(__dirname, "public"));
 // bring body parser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
