50a4aa68d193dddd25acfe6f75e5ff1b55126237
slmgnm <saleemghanem@gmail.com>
2023-01-24 14:53:49 +0200

    views

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 app.use(express.static("uploads"));
 app.use(express.static("node_modules"));
+app.use(express.static("views"));
+
 // session and flash config .
 app.use(
   session({
@@ -28,7 +30,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: { maxAge: 60000 * 15 },
-  }),
+  })
 );
 app.use(flash());
 // bring passport
@@ -48,13 +50,13 @@ app.get("/", (req, res) => {
 // bring events routes
 
 const events = require("./routes/event-routes");
-// app.use("/events", events);
-app.use(express.static('views' + '/events', events));
+app.use("/events", events);
+// app.use(express.static('views' + '/events', events));
 // bring user routes
 const users = require("./routes/user-routes");
 const router = require("./routes/event-routes");
-// app.use("/users", users);
-app.use(express.static('views' + '/users', users));
+app.use("/users", users);
+// app.use(express.static('views' + '/users', users));
 // listen to port 8080
 
 const PORT = process.env.PORT || 8080;
