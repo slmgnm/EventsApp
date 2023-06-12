687f81d8b1af435090cadafb57617b5ce5894ca4
SALIM GHANEM <saleemghanem@gmail.com>
2023-06-12 12:00:42 +0300

    __dirname

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -6,7 +6,7 @@ const multer = require("multer");
 // configure multer
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, null, __dirname + "uploads/images");
+    cb(null, null, __dirname + "images");
   },
   filename: function (req, file, cb) {
     cb(null, file.fieldname + "-" + Date.now() + ".png");
